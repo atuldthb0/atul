6d2fb2c1ef226a94af0e38688c99f5a5376018fa
atuldthb0 <thebigocommunity@disroot.org>
2026-04-08 06:49:45 +0530

    feat: update menu items with real content and images
    Replaced placeholder menu items with actual project content including:
    - Aurum, Ati, Atulya, Goldbeard, and Simba entries
    - Personal moments and images from /atul directory
    - Updated descriptions with meaningful text
    - All items now link to https://atul.thebigocommunity.org
    
    Also updated bun.lock configVersion from 0 to 1 and fixed minor formatting.

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,96 @@
-"use client"
+"use client";
 import InfiniteMenu from "@/components/InfiniteMenu";
 
 const items = [
   {
-    image: "https://picsum.photos/300/300",
-    link: "https://google.com/",
-    title: "Item 1",
-    description: "This is pretty cool, right?",
+    image: "/apps/Aurum.png",
+    link: "https://atul.thebigocommunity.org",
+    title: "Aurum",
+    description: "Redirect. Transform. Rise.",
   },
   {
-    image: "https://picsum.photos/400/400?grayscale",
-    link: "https://google.com/",
-    title: "Item 2",
-    description: "This is pretty cool, right?",
+    image: "/atul/Ati.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Ati",
+    description: "The original legend!",
   },
   {
-    image: "https://picsum.photos/500/500?grayscale",
-    link: "https://google.com/",
-    title: "Item 3",
-    description: "This is pretty cool, right?",
+    image: "/atul/Ati.png",
+    link: "https://atul.thebigocommunity.org",
+    title: "Ati",
+    description: "Same vibe, transparent edition!",
   },
   {
-    image: "https://picsum.photos/600/600?grayscale",
-    link: "https://google.com/",
-    title: "Item 4",
-    description: "This is pretty cool, right?",
+    image: "/atul/Atl.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Ati",
+    description: "They are beautiful because you are.",
+  },
+  {
+    image: "/atul/atulya.png",
+    link: "https://atul.thebigocommunity.org",
+    title: "Atulya",
+    description: "The Sanskrit upgrade!",
+  },
+  {
+    image: "/atul/goldbeard.png",
+    link: "https://atul.thebigocommunity.org",
+    title: "Goldbeard",
+    description: "Pirate by day, Pirate by night!",
+  },
+  {
+    image: "/atul/IMG_2561.JPG",
+    link: "https://atul.thebigocommunity.org",
+    title: "atul",
+    description: "Late night Shoot.",
+  },
+  {
+    image: "/atul/IMG_2567.JPG",
+    link: "https://atul.thebigocommunity.org",
+    title: "Moment #2567",
+    description: "Debugging at 3 AM!",
+  },
+  {
+    image: "/atul/IMG_2607.JPG",
+    link: "https://atul.thebigocommunity.org",
+    title: "Moment",
+    description: "PM asks for small change!",
+  },
+  {
+    image: "/atul/IMG_20260207_024035.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Future Memory",
+    description: "From the future!",
+  },
+  {
+    image: "/atul/IMG20241217102404.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Atul Raj",
+    description: "Founder who finds what your soul always needed.",
+  },
+  {
+    image: "/atul/Simba 2.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Simba",
+    description: "The sequel!",
+  },
+  {
+    image: "/atul/Simba.JPG",
+    link: "https://atul.thebigocommunity.org",
+    title: "Simba",
+    description: "Respect the legend!",
+  },
+  {
+    image: "/atul/Simba.jpg",
+    link: "https://atul.thebigocommunity.org",
+    title: "Simba",
+    description: "King of the the Jungle!",
   },
 ];
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <InfiniteMenu items={items} scale={1} />
     </div>
   );
