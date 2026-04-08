5dd4230c3b86ffc5a36b268ec15c984c29a1da35
atuldthb0 <thebigocommunity@disroot.org>
2026-04-08 07:38:32 +0530

    feat: add dark class to root html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable, "dark")}
     >
       <body className="h-full w-full overflow-hidden">{children}</body>
     </html>
